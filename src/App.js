f6060f8a65aa13263fd90c7b650518b75d3e42b9
Esref Gunaydin <esrefgunaydin@gmail.com>
2020-12-12 14:22:45 -0800

    table added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState, useEffect } from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import BasicTable from './components/Table';
 
 function App() {
 
@@ -10,9 +11,11 @@ function App() {
   return (
     <div className="App">
 
-        <Button onClick={()=> setCounter(counter + 1)} variant="contained" color="primary">+</Button>
-        <Button onClick={()=> setCounter(counter - 1)} variant="contained" color="secondary">-</Button>
-        <h2>{counter}</h2>
+        <Button onClick={()=> setCounter(counter + 1)} variant="contained" color="primary" m={5}>+</Button>
+        <Button onClick={()=> setCounter(counter - 1)} variant="contained" color="secondary" m={5}>-</Button>
+        <h1>{counter}</h1>
+        <hr/>
+        <BasicTable></BasicTable>
     </div>
   );
 }
